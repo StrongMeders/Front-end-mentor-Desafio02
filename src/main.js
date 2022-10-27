bb30360b8ba78c575c29a740f52b0aab940a29a2
StrongMeders <strongmeders@gmail.com>
2022-10-27 17:51:08 -0300

    macara aplicada no cvc

--- src/main.js
+++ src/main.js
@@ -1,3 +1,10 @@
-import "./css/index.css"
+import "./css/index.css";
 import IMask from 'imask';
+
+const securityCode = document.querySelector("#security-code")
+const securityCodePattern = {
+  mask: "000",
+}
+const securityCodeMasked = IMask(securityCode, securityCodePattern)
+
 
